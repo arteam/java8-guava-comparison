77e687343c002120880217e38bbadd15d1d4b4e8
Artem Prigoda <arteamon@gmail.com>
2015-04-20 23:36:20 +0300

    Add Maps#difference

--- src/test/java/com/github/arteam/jgcompare/MapsTest.java
+++ src/test/java/com/github/arteam/jgcompare/MapsTest.java
@@ -2,14 +2,17 @@ package com.github.arteam.jgcompare;
 
 import com.google.common.collect.*;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.data.MapEntry;
 import org.junit.Test;
 
 import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.MapEntry.entry;
 
 /**
  * Date: 5/15/14
@@ -78,5 +81,32 @@ public class MapsTest {
 
         assertThat(Maps.asMap(source, teams::get)).isEqualTo(expected);
         assertThat(source.stream().collect(Collectors.toMap(Function.identity(), teams::get))).isEqualTo(expected);
+    }
+
+    @Test
+    public void testDifference() {
+        Map<Integer, String> teams = ImmutableMap.<Integer, String>builder()
+                .put(21, "Boston Bruins")
+                .put(24, "Los Angeles Kings")
+                .put(12, "Chicago Blackhawks")
+                .put(42, "St. Louis Blues")
+                .put(29, "Arizona Coyotes")
+                .put(92, "Winnipeg Jets")
+                .put(88, "Colorado Avalanche")
+                .build();
+        Map<Integer, String> stanleyCupWinners = ImmutableMap.<Integer, String>builder()
+                .put(21, "Boston Bruins")
+                .put(24, "Los Angeles Kings")
+                .put(12, "Chicago Blackhawks")
+                .put(88, "Colorado Avalanche")
+                .build();
+        assertThat(Maps.difference(teams, stanleyCupWinners).entriesOnlyOnLeft())
+                .contains(entry(42, "St. Louis Blues"), entry(92, "Winnipeg Jets"), entry(29, "Arizona Coyotes"));
+
+        assertThat(teams.entrySet()
+                .stream()
+                .filter(e -> !stanleyCupWinners.containsKey(e.getKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
+                .contains(entry(42, "St. Louis Blues"), entry(92, "Winnipeg Jets"), entry(29, "Arizona Coyotes"));
     }
 }
