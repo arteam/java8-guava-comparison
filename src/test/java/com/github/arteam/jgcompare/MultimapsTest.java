33bc8b378ee4db92c608eee7d9fafb8820ed179a
Artem Prigoda <arteamon@gmail.com>
2015-05-06 22:08:36 +0300

    Add Multimaps#fromMap

--- src/test/java/com/github/arteam/jgcompare/MultimapsTest.java
+++ src/test/java/com/github/arteam/jgcompare/MultimapsTest.java
@@ -30,20 +30,41 @@ public class MultimapsTest {
             .add(of("New York Rangers", "Metropolitan"))
             .add(of("Dallas Stars", "Central"))
             .build();
+    ImmutableListMultimap<String, TeamDivision> teamsByDivision = ImmutableListMultimap.<String, TeamDivision>builder()
+            .putAll("Pacific", of("Calgary Flames", "Pacific"), of("Vancouver Canucks", "Pacific"))
+            .putAll("Atlantic", of("Toronto Maple Leafs", "Atlantic"), of("Florida Panthers", "Atlantic"))
+            .putAll("Metropolitan", of("Detroit Red Wings", "Metropolitan"), of("New York Rangers", "Metropolitan"))
+            .putAll("Central", of("Nashville Predators", "Central"), of("Dallas Stars", "Central"))
+            .build();
+    ;
 
     @Test
     public void testIndex() {
-        assertThat(Multimaps.index(teams, TeamDivision::getDivision)).isEqualTo(ImmutableMultimap.<String, TeamDivision>builder()
-                .putAll("Pacific", of("Calgary Flames", "Pacific"), of("Vancouver Canucks", "Pacific"))
-                .putAll("Atlantic", of("Toronto Maple Leafs", "Atlantic"), of("Florida Panthers", "Atlantic"))
-                .putAll("Metropolitan", of("Detroit Red Wings", "Metropolitan"), of("New York Rangers", "Metropolitan"))
-                .putAll("Central", of("Nashville Predators", "Central"), of("Dallas Stars", "Central"))
-                .build());
+        assertThat(Multimaps.index(teams, TeamDivision::getDivision)).isEqualTo(teamsByDivision);
         assertThat(teams.stream().collect(Collectors.groupingBy(TeamDivision::getDivision)))
                 .contains(entry("Pacific", ImmutableList.of(of("Calgary Flames", "Pacific"), of("Vancouver Canucks", "Pacific"))))
                 .contains(entry("Atlantic", ImmutableList.of(of("Toronto Maple Leafs", "Atlantic"), of("Florida Panthers", "Atlantic"))))
                 .contains(entry("Metropolitan", ImmutableList.of(of("Detroit Red Wings", "Metropolitan"), of("New York Rangers", "Metropolitan"))))
                 .contains(entry("Central", ImmutableList.of(of("Nashville Predators", "Central"), of("Dallas Stars", "Central"))));
+    }
+
+    @Test
+    public void testFromMap() {
+        ImmutableMap<String, String> topTeams = ImmutableMap.of("Pacific", "Anaheim", "Atlantic", "Montreal",
+                "Metropolitan", "New York", "Central", "St. Louis");
+        assertThat(Multimaps.forMap(topTeams)).isEqualTo(ImmutableSetMultimap.<String, String>builder()
+                .put("Pacific", "Anaheim")
+                .put("Atlantic", "Montreal")
+                .put("Metropolitan", "New York")
+                .put("Central", "St. Louis")
+                .build());
+        assertThat(topTeams.entrySet().stream()
+                .collect(Collectors.groupingBy(Map.Entry::getKey,
+                        Collectors.mapping(Map.Entry::getValue, Collectors.toList()))))
+                .contains(entry("Pacific", ImmutableList.of("Anaheim")))
+                .contains(entry("Atlantic", ImmutableList.of("Montreal")))
+                .contains(entry("Metropolitan", ImmutableList.of("New York")))
+                .contains(entry("Central", ImmutableList.of("St. Louis")));
 
     }
 }
