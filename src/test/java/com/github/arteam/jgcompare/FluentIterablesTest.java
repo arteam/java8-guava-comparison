2f03aa127421b5ee36541b1a0a42431b001cd065
Artem Prigoda <arteamon@gmail.com>
2015-05-28 23:18:31 +0300

    Add FluentIterable#index

--- src/test/java/com/github/arteam/jgcompare/FluentIterablesTest.java
+++ src/test/java/com/github/arteam/jgcompare/FluentIterablesTest.java
@@ -1,14 +1,13 @@
 package com.github.arteam.jgcompare;
 
 import com.google.common.base.Splitter;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
+import com.google.common.collect.*;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Spliterators;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -38,4 +37,24 @@ public class FluentIterablesTest {
                 .collect(Collectors.toList())).containsOnly(1, 18, 20, 24, 9, 92);
     }
 
+
+    @Test
+    public void index() {
+        Splitter splitter = Splitter.on(" ");
+        assertThat(FluentIterable.from(source)
+                .transformAndConcat(splitter::split)
+                .index(s -> s.substring(0, 1))).isEqualTo(ImmutableListMultimap.<String, String>builder()
+                .putAll("1", "1", "18")
+                .putAll("2", "20", "24")
+                .putAll("9", "9", "92")
+                .build());
+
+        assertThat(stream.flatMap(s -> Stream.of(s.split(" ")))
+                .collect(Collectors.groupingBy(s -> s.substring(0, 1))))
+                .isEqualTo(ImmutableMap.<String, List<String>>builder()
+                        .put("1", ImmutableList.of("1", "18"))
+                        .put("2", ImmutableList.of("20", "24"))
+                        .put("9", ImmutableList.of("9", "92"))
+                        .build());
+    }
 }
